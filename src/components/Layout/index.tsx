3bd3081eda85ae9df1bfc3bf7dec5247df66b178
r3nanp <renanmol87@gmail.com>
2020-11-05 20:49:38 -0300

    feat (layout): create layout component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,16 @@
+import Footer from '../Footer'
+import Header from '../Header'
+
+import { Container } from './styles'
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <Header />
+
+      <Footer />
+    </Container>
+  )
+}
+
+export default Layout
