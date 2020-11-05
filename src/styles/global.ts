c11e7a06be8c089aa7f0e981c311eebeaf46913c
r3nanp <renanmol87@gmail.com>
2020-11-05 20:47:20 -0300

    feat (theme): create the global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    background: var(--black);
+    color: var(--white);
+    font: 400 16px Roboto, system-ui, sans-serif;
+  }
+  input, button {
+    outline: none;
+    border: none;
+  }
+
+  :root {
+    --primary: ${props => props.theme.colors.primary}
+    --white: ${props => props.theme.colors.white}
+    --black: ${props => props.theme.colors.black}
+    --blackLighter: ${props => props.theme.colors.blackLighter}
+    --grayLight: ${props => props.theme.colors.grayLight}
+    --grayMedium: ${props => props.theme.colors.grayMedium}
+    --lightblue: ${props => props.theme.colors.lightblue}
+    --green: ${props => props.theme.colors.green}
+    --outline: ${props => props.theme.colors.outline}
+  }
+`
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+const theme = {
+  colors: {
+    primary: '#2A7AE4',
+    black: '#000000',
+    blackLighter: '#9E9E9E',
+    grayLight: '#F5F5F5',
+    grayMedium: '#e5e5e5', 
+    white: '#FFFFFF',
+    lightblue: '#6BD1FF',
+    green: '#00C86F',
+    outline: '#2F3336',
+  }
+}
+
+export default theme
