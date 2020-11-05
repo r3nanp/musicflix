6f568025f57edca4dd6d44d26d54ec24a83cfb04
r3nanp <renanmol87@gmail.com>
2020-11-05 20:17:41 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>MusicFlix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>testing...</h1>
+      </main>
+        
+    </>
+  )
+}
