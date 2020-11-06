67e218d318c6b94496049845abed624f4b6c67f2
r3nanp <renanmol87@gmail.com>
2020-11-05 22:09:46 -0300

    style (Layout): add the styles

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -1,5 +1,8 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
 
-`;
+  background: var(--background);
+`
