c4333467d8ecf3b611b600adb242f5c25c52b412
r3nanp <renanmol87@gmail.com>
2020-11-05 22:10:53 -0300

    feat (app): add the theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,16 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyles from '../styles/global'
+import theme from '../styles/theme'
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
