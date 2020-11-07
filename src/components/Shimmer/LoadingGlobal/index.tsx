2ff945a2f017a58e41a869491a27114ce6d29857
r3nanp <renanmol87@gmail.com>
2020-11-07 19:01:12 -0300

    feat (loading global): create the global loading with shimmer effect

---
+++ src/components/Shimmer/LoadingGlobal/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles'
+import Skeleton from '../../Skeleton'
+
+export default function LoadingGlobal () {
+  return (
+    <Container>
+      <Skeleton />
+    </Container>
+  )
+}
