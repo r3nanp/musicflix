676bcd2fbf2eb3b2b96bb77931f170fe93aa7352
r3nanp <renanmol87@gmail.com>
2020-11-07 19:05:24 -0300

    feat (index): put dynamic data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import Head from 'next/head'
 
-import BannerMain from '../components/BannerMain'
 import Layout from '../components/Layout'
+import BannerMain from '../components/BannerMain'
+import Carousel, { CarouselOptions } from '../components/Carousel'
+import LoadingGlobal from '../components/Shimmer/LoadingGlobal'
+
+import useApi from '../Hooks/useApi'
 
 export default function Home() {
+  const { data } = useApi<CarouselOptions[]>('categories?_embed=videos')
+
+  if (!data) {
+    return <LoadingGlobal />
+  }
+
   return (
     <>
       <Head>
@@ -13,10 +23,17 @@ export default function Home() {
 
       <Layout>
         <BannerMain
-          url={`https://www.youtube.com/watch?v=BjC0KUxiMhc`}
-          videoTitle={`Ode To The Mets`}
+          videoTitle={'Ode To The Mets'}
+          url={`https://youtu.be/BjC0KUxiMhc`}
         />
 
+        {data?.map((category, index: number) => {
+          return (
+            <div key={index}>
+              <Carousel ignoreFirstVideo category={category} />
+            </div>
+          )
+        })}
       </Layout>
     </>
   )
