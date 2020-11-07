30e327affaf78cdee8987395dcfb2d5e9569f27c
r3nanp <renanmol87@gmail.com>
2020-11-06 21:43:44 -0300

    feat (index): add the banner main and the settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,23 @@
+import Head from 'next/head'
+
+import BannerMain from '../components/BannerMain'
 import Layout from '../components/Layout'
 
 export default function Home() {
   return (
     <>
-      <Layout />
+      <Head>
+        <title>MusicFlix</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+
+      <Layout>
+        <BannerMain
+          url={`https://www.youtube.com/watch?v=BjC0KUxiMhc`}
+          videoTitle={`Ode To The Mets`}
+        />
+
+      </Layout>
     </>
   )
 }
