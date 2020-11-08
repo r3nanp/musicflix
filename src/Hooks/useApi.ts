98380cef7aa24d1ac9f8eb222009e395943294d4
r3nanp <renanmol87@gmail.com>
2020-11-08 16:58:57 -0300

    feat (useApi): remove swr

--- src/Hooks/useApi.ts
+++ src/Hooks/useApi.ts
@@ -1,13 +1,8 @@
-import useSWR from 'swr'
 import api from '../services/axios'
 
-export default function useApi<Data = any, Error = any>(url: string) {
-  const { data, error } = useSWR<Data, Error>(url, async url => {
-    const response = await api.get(url)
-    const data = await response.data
-
-    return data
-  })
-
-  return { data, error }
+export default async function fetchData<Data = any>(url: string) {
+  const response = await api.get<Data>(url)
+  const data = response.data
+  
+  return data
 }
