99432fd2162a9c8bc7176d11f715b048132a292c
r3nanp <renanmol87@gmail.com>
2020-11-30 16:07:31 -0300

    refactor: fix lint problems

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import Head from 'next/head'
-import fetchData from '../Hooks/useApi'
 import { GetServerSideProps } from 'next'
+import { DataOptions } from '../@types'
+import { fetchData } from '../hooks/useApi'
 
 import Layout from '../components/Layout'
 import BannerMain from '../components/BannerMain'
 import Carousel from '../components/Carousel'
 import LoadingGlobal from '../components/Shimmer/LoadingGlobal'
 
-export default function Home ({ data }) {
-
+function Home({ data }): JSX.Element {
   if (!data) {
     return <LoadingGlobal />
   }
@@ -25,13 +26,11 @@ export default function Home ({ data }) {
           url="https://youtu.be/BjC0KUxiMhc"
         />
 
-        {data.map((category, index: number) => {
-          return (
-            <div key={index}>
-              <Carousel category={category} />
-            </div>
-          )
-        })}
+        {data.map((category, index: number) => (
+          <div key={index}>
+            <Carousel category={category} />
+          </div>
+        ))}
       </Layout>
     </>
   )
@@ -43,6 +42,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       data,
-    }
+    },
   }
-} 
+}
+
+export default Home
