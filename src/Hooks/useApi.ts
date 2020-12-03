82c617bab3b8e45b9c1f969efa7f2712f8a75d0d
r3nanp <renanmol87@gmail.com>
2020-12-03 13:49:52 -0300

    refactor

--- src/Hooks/useApi.ts
+++ src/Hooks/useApi.ts
@@ -1,11 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import api from '../services/axios'
 
-async function fetchData<Data = any>(url: string) {
+export async function fetchData<Data = any>(url: string) {
   const response = await api.get<Data>(url)
   const data = response.data
 
   return data
 }
-
-export { fetchData }
